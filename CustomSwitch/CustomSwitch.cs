32d72782d1b2a606cf788668398ddf6974674431
Abdul Muhaymin <mampary@hotmail.com>
2018-03-25 16:51:34 +0530

    Custom Switch for Xamarin.Forms
    The original Xamarin Forms Switch fires Toggled Event when switch is changed both in code / by user.
    
    Custom Switch allows to understand who invoked this operation, whether it was user initiated or code initiated

---
+++ CustomSwitch/CustomSwitch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CustomSwitch
+{
+    public class CustomToggledEventArgs : EventArgs
+    {
+        public CustomToggledEventArgs(bool value, bool isuser)
+        {
+            Value = value;
+            IsUser = isuser;
+        }
+
+        public bool Value { get; private set; }
+        public bool IsUser { get; private set; }
+    }
+
+
+    public class CustomSwitch : Switch
+    {
+        private bool isUser { get; set; } = true;
+        public bool IsCustomToggled
+        {
+            get => IsToggled;
+            set
+            {
+                //The Order of Code is Very Important, We are removing Event Handler from Original Switch
+                this.Toggled -= Handle_Toggled;
+                //Setting IsToggled with IsCustomToggled
+                IsToggled = value;
+                //Invoking Custom Event with is User Property
+                CustomToggled?.Invoke(this, new CustomToggledEventArgs(IsToggled, false));
+                //Enabling Event again
+                this.Toggled += Handle_Toggled;
+            }
+        }
+
+        public static readonly BindableProperty IsCustomProperty = BindableProperty.Create("IsCustomToggled", typeof(bool), typeof(CustomSwitch), false, BindingMode.TwoWay);
+
+        public EventHandler<CustomToggledEventArgs> CustomToggled { get; set; }
+
+
+        public CustomSwitch()
+        {
+            //Subscribing to the original event
+            this.Toggled += Handle_Toggled;
+        }
+
+        private void Handle_Toggled(object sender, ToggledEventArgs e)
+        {
+            //This even only fires when user is changing switch
+            CustomToggled?.Invoke(this, new CustomToggledEventArgs(IsToggled, true));
+        }
+    }
+}
---
+++ CustomSwitch/CustomSwitchPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CustomSwitch
+{
+    public partial class CustomSwitchPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            custom.IsCustomToggled = !custom.IsCustomToggled;
+        }
+
+        public CustomSwitchPage()
+        {
+            InitializeComponent();
+            //switchee.Toggled += Switche_toggled;
+            custom.CustomToggled += thenga;
+        }
+
+        //private void Switche_toggled(object sender, ToggledEventArgs e)
+        //{
+        //    var val = switchee.IsToggled;
+        //}
+        private void thenga(object sender, CustomToggledEventArgs e)
+        {
+            var val = custom.IsCustomToggled;
+            DisplayAlert("Switch Control", (e.IsUser ? "I'm User" : "I'm program") + "& my value is " + e.Value, "Okay");
+        }
+    }
+}
