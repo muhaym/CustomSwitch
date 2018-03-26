785f000530730e08d5a3c24e57ad1f3aaf791be0
Abdul Muhaymin <mampary@hotmail.com>
2018-03-26 16:50:17 +0530

    Fixed Simple

--- CustomSwitch/CustomSwitchPage.xaml.cs
+++ CustomSwitch/CustomSwitchPage.xaml.cs
@@ -5,6 +5,11 @@ namespace CustomSwitch
 {
     public partial class CustomSwitchPage : ContentPage
     {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            custom.IsCustomToggled = !custom.IsCustomToggled;
+        }
+
         void Handle_CustomToggled(object sender, CustomToggledEventArgs e)
         {
             DisplayAlert("Switch Control", (e.IsUser ? "I'm User" : "I'm program") + "& my value is " + e.Value, "Okay");
@@ -14,6 +19,7 @@ namespace CustomSwitch
         {
             InitializeComponent();
         }
+
 
     }
 }
